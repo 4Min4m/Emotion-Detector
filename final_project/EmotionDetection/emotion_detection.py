9efec5084af0e9f48fb3f104a5c207846c915f9a
Amin <36745993+4Min4m@users.noreply.github.com>
2025-02-13 12:34:41 +0100

    Update emotion_detection.py

--- final_project/EmotionDetection/emotion_detection.py
+++ final_project/EmotionDetection/emotion_detection.py
@@ -3,6 +3,15 @@ import requests
 import json
 
 def emotion_detector(text_to_analyze):
+    if not text_to_analyze:  # Check for blank input
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = {"raw_document": {"text": text_to_analyze}}
